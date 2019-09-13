19a75031d516b290d58d8a40bb70028babcdeab8
Francesco Ceccon <francesco@ceccon.me>
2019-09-13 19:54:12 +0100

    Fix style cop issue

---
+++ stellar-dotnet-sdk-test/requests/FakeResponse.cs
@@ -0,0 +1,19 @@
+using System.Net.Http;
+using System.Threading;
+
+namespace stellar_dotnet_sdk_test.requests
+{
+    public abstract class FakeResponse
+    {
+        public abstract HttpResponseMessage MakeResponse(CancellationToken cancellationToken);
+
+        protected FakeResponse()
+        {
+        }
+
+        public static FakeResponse StartsStream(params StreamAction[] actions)
+        {
+            return new FakeResponseWithStream(actions);
+        }
+    }
+}
